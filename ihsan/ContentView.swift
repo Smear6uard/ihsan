c729c41a45906deb6800566596f75b114faec55a
Sameer Akhtar <sakhtar9@depaul.edu>
2026-05-09 03:09:49 -0500

    Initial Commit

---
+++ ihsan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ihsan
+//
+//  Created by Sameer Akhtar on 5/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ihsan/ihsanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ihsanApp.swift
+//  ihsan
+//
+//  Created by Sameer Akhtar on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ihsanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
